698f112dea15487461478c3e62cef466ca3275a5
vtu81 <48283858+vtu81@users.noreply.github.com>
2021-06-01 21:46:57 +0800

    Test file for type checking

---
+++ test/type_check.c
@@ -0,0 +1,15 @@
+int main()
+{
+    int x, y, z;
+    char a = 'c';
+    double d = 3.14, e;
+    int array1[30];
+    int array2[30][30][30];
+    3 + 4;
+    3.14 + 4;
+    3.14 + 4.14;
+    z + 3;
+    a + 3;
+    x + y;
+    x | 0;
+}
