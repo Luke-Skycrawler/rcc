3b94e1fa129b847f294703be3cf298d8b98cc8ac
Shay P.C <3180102686@zju.edu.cn>
2021-06-04 17:53:53 +0800

    [doge] a better gramma ( incto -> to, decto -> downto

--- test/for_test.c
+++ test/for_test.c
@@ -1,19 +1,19 @@
 int main()
 {
     int k = 3;
-    for(i: 3 incto 5)
+    for(i: 3 to 5)
     {
         printf("i = %d\n", i);
     }
     // printf("(Illegal access) i = %d\n", i); // Uncomment this line and you'll see errors
     
-    for(k: k * 3 decto 5)
+    for(k: k * 3 downto 5)
     {
         printf("k = %d\n", k);
     }
     printf("Original k = %d\n", k);
     
-    for(j: 3 incto 2) // won't enter the following loop 
+    for(j: 3 to 2) // won't enter the following loop 
     {
         printf("j = %d\n", j);
     }
